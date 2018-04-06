e8f7e842618a454f0d8ef1da4c0649b0d131f101
cd3770rn <email@email.com>
2018-04-05 20:09:42 -0500

    Lab complete

---
+++ src/main/java/com/alex/webapptemplatescorrectversion/WebapptemplatescorrectversionApplication.java
@@ -0,0 +1,27 @@
+package com.alex.webapptemplatescorrectversion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
+import java.util.List;
+
+@SpringBootApplication
+@Controller
+public class WebapptemplatescorrectversionApplication {
+
+	@RequestMapping("/")
+	public String home(Model model) {
+	    List favoriteFoods = Arrays.asList("pizza", "tacos", "hamburgers");
+        model.addAttribute("author", "Alex");
+	    model.addAttribute("favoriteFoods", favoriteFoods);
+		return "index.html";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebapptemplatescorrectversionApplication.class, args);
+	}
+}
